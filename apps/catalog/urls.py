b455b75e93906e41a7638d86d234c110445d25e1
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-04-27 17:09:29 +0200

    Ajout de l'url de product_detail

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from.views import index
+from.views import index, product_detail
 
 urlpatterns = [
-    path("", index, name="catalog-index")
+    path("", index, name="catalog-index"),
+    path("product/<str:slug>/", product_detail, name="catalog-product")
 ]
