d7d9e3758b4f12a69d84999b8d332ba03a53bd92
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-04-27 16:58:05 +0200

    Ajout de slug dans Product

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -14,6 +14,7 @@ class Category(models.Model):
     
 class Product(models.Model):
     name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64)
     thumbnail = models.ImageField(upload_to="products", blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1) 
     price = models.FloatField(default=0.0, validators=[MinValueValidator(0)])
