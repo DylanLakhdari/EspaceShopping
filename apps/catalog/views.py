d21a7e1cee8fb187612db136ea30c8e76ede1e5e
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-04-27 17:07:07 +0200

    Ajout de la view product_detail

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Product
 
 def index(request):
     products = Product.objects.all()
 
     return render(request, "catalog/index.html", context={"products": products})
+
+def product_detail(request, slug):
+
+    products = get_object_or_404(Product, slug=slug)
+
+    return render(request, "product_detail.html", context={"products": products})
