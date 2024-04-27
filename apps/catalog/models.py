c1b48a8e36a4c241ee6349b933bae3ba114deed1
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-04-27 16:22:46 +0200

    Ajout du model Product

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 class Category(models.Model): 
-    name = models.CharField(max_length=50) 
+    name = models.CharField(max_length=64) 
   
     @staticmethod
     def get_all_categories(): 
         return Category.objects.all() 
   
+    def __str__(self): 
+        return self.name 
+    
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    thumbnail = models.ImageField(upload_to="products", blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1) 
+    price = models.FloatField(default=0.0, validators=[MinValueValidator(0)])
+    stock = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    description = models.TextField(blank=True)
+    
     def __str__(self): 
         return self.name 
