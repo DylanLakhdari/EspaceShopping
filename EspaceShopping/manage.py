1f891ef247b8897b54844f96025be7e32d06dbd2
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-04-27 11:42:40 +0200

    Rennomage du projet

--- EspaceShopping/manage.py
+++ EspaceShopping/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AutoLocation.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EspaceShopping.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
