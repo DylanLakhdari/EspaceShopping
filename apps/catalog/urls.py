53bdd96ae861e9e766226121b1704b1c41e5bd07
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-04-29 20:13:54 +0200

    Ajout de l'url du panier

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from.views import index, product_detail, add_to_cart
+from.views import index, product_detail, add_to_cart, cart
 
 urlpatterns = [
     path("", index, name="catalog-index"),
     path("product/<str:slug>/", product_detail, name="catalog-product"),
     path("product/<str:slug>/add-to-cart/", add_to_cart, name="catalog-add-to-cart"),
+    path("cart/", cart, name="catalog-cart"),
 ]
