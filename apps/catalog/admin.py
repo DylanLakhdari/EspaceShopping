2b7163ecfd078319b2a6df9ca77ff11dcc79aa8f
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-04-29 18:50:29 +0200

    Ajout du model Cart de l'app catalog

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Category, Product, Order
+from .models import Category, Product, Order, Cart
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Cart)
