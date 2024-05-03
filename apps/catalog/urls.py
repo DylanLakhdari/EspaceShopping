9aa938819d95abbedd72f32076ac717e6141eef8
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-05-03 19:36:23 +0200

    Ajout de l'url de delete_order

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from.views import index, product_detail, add_to_cart, cart, delete_cart
+from.views import index, product_detail, add_to_cart, cart, delete_cart, delete_order
 
 urlpatterns = [
     path("", index, name="catalog-index"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path("product/<str:slug>/add-to-cart/", add_to_cart, name="catalog-add-to-cart"),
     path("cart/", cart, name="catalog-cart"),
     path("cart/delete/", delete_cart, name='catalog-delete-cart'),
+    path("cart/order/delete/<int:order_id>", delete_order, name="catalog-delete-cart-order"),
 ]
