9f7c19599820de2190c11102511a22afc673f4ec
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-05-04 21:08:20 +0200

    Message de l'erreur unique

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     pass
-    email = models.EmailField(unique=True)
+    email = models.EmailField(
+        error_messages={'unique': 'Cet email existe déjà'}, unique=True)
 
     def __str__(self):
         return self.username
