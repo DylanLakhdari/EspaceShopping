36b0ca0ceb11695b898597231ebbb81e4525f1a1
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-05-07 19:07:33 +0200

    Url de user_settings

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import signup
+from .views import signup, user_settings
 
 urlpatterns = [
-    path("signup/", signup.as_view(), name="accounts-signup")
+    path("signup/", signup.as_view(), name="accounts-signup"),
+    path("settings/<str:username>/", user_settings, name = 'accounts-user-settings')
 ]
