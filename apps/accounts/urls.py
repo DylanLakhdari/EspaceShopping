63233fad069fadd9ee0e951f3245db3309e8c16d
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-05-08 18:00:57 +0200

    Url de user_delete

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import signup, user_settings
+from .views import signup, user_settings, user_delete
 
 urlpatterns = [
     path("signup/", signup.as_view(), name="accounts-signup"),
-    path("settings/<str:username>/", user_settings, name = 'accounts-user-settings')
+    path("settings/<str:username>/", user_settings, name = 'accounts-user-settings'),
+    path("user-delete/", user_delete, name="accounts-user-delete")
 ]
