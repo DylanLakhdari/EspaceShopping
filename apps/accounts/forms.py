e9bd7b18629fcc91066925f1397ca7375bd2e06a
Dylan Lakhdari <emmelf.exe@gmail.com>
2024-05-13 23:40:10 +0200

    Ajout du formulaire PurchaseForm

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -13,3 +13,6 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('first_name', 'last_name', "profile_picture")
+
+class PurchaseForm:
+    fields = ('adress','first_name', 'last_name')
